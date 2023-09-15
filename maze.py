9fae5bfefb10a511701512aeb767019538d45a12
redcommander561 <126519219+redcommander561@users.noreply.github.com>
2023-09-15 12:01:40 -0700

    made some improvements but its still crashing

--- maze.py
+++ maze.py
@@ -1,6 +1,15 @@
 import turtle
+import time
 
 s = turtle.getscreen()
 s.bgpic('maze.gif')
 t = turtle.Turtle()
+time.sleep(30)
 
+import turtle
+s = turtle.getscreen()
+t= turtle.Turtle
+t.fd(200)
+t.forward(50)
+t.lt(30)
+t.forward(35)
